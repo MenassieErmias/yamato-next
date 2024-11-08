40cb7b3b685b887295541af6fcf865562173a593
MenassieErmias <ermiasmenase@gmail.com>
2024-11-08 11:26:04 +0300

    modified navbar to add a responsive menu, logic not done yet: Navbar.jsxx, added styles to Navbar component: navbar.modules.css, added react-icons package: package.json

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,16 +1,40 @@
+'use client'
 import React from 'react'
 import Link from "next/link"
+import './../nav.modules.css'
+import { useState } from 'react'
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
-  let linkStr = ""
+
+  const [mobileMenuOn, setMobileMenuOn] = useState(false);
   return (
-    <nav>
-      <h1>Yamato Engineering</h1>
-      <Link href="/services">Services</Link>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
-    </nav>
+    <nav className='nav'>
+      <div className="nav-container">
+        <h1 className='nav-header'>Yamato Engineering</h1>
+
+        <button
+          className='mobile-menu-btn'
+          onClick={(mobileMenuOn) => setMobileMenuOn(!mobileMenuOn)}
+        >
+          <RxHamburgerMenu className='hamburget-icon' />
+        </button>
+        <ul className="nav-links-list display-none">
+          <li className="nav-link-item">
+            <Link href="/services">Services</Link>
+          </li>
+          <li className="nav-link">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="nav-link">
+            <Link href="/about">About</Link>
+          </li>
+          <li className="nav-link">
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav >
   )
 }
 
